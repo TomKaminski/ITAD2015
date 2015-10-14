df794f11bb9e627a1fdaea322a35eaf767a7e452
Tomasz Kamiński <T.Kaminski@lgbs.pl>
2015-10-14 17:46:07 +0200

    test

---
+++ Itad2015/Hubs/CheckInHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
+
+namespace Itad2015.Hubs
+{
+    public class CheckInHub:Hub
+    {
+        private readonly static Dictionary<string,Connections> InMemoryConnections = new Dictionary<string, Connections>();
+    }
+
+    public class Connections
+    {
+        public Guid DeviceConnectionId { get; set; }
+        public Guid UserConnectionId { get; set; }
+    }
+}
